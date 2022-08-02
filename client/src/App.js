30cff68708405dcc4844956b0fa80ef469c7ff29
supahkenneh <chunghlken@gmail.com>
2022-08-02 12:16:46 -0400

    return refresh token

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,12 @@ function App() {
     const urlParams = new URLSearchParams(queryString);
     const accessToken = urlParams.get('access_token');
     const refreshToken = urlParams.get('refresh_token');
-
-    console.log(accessToken);
-    console.log(refreshToken);
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+    }
   }, [])
   return (
     <div className="App">
@@ -22,8 +25,6 @@ function App() {
         <a
           className="App-link"
           href="http://localhost:8888/login"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           Log in to Spotify
         </a>
