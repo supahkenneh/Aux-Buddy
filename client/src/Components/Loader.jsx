8565ddfba4b40c76f7d4e37186991c4076173de3
supahkenneh <chunghlken@gmail.com>
2022-08-11 18:30:22 -0400

    edit loader style

--- client/src/Components/Loader.jsx
+++ client/src/Components/Loader.jsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div className='flex justify-center w-screen h-screen left-0 top-0 fixed z-40'>
+    <div className='flex justify-center w-screen h-screen left-0 top-0 fixed z-40 bg-spotify-dark/50'>
       <div className='flex justify-evenly h-1/6 self-center load-container'>
         <div className='bg-white/50 bar1 w-8 mx-1'></div>
         <div className='bg-white/50 bar2 w-8 mx-1'></div>
